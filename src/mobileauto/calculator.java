da56f63a12cf2e010a2ac1878344250a88556513
mohdazad.khan <mohdazad.khan@INL-7030YC2.groupinfra.com>
2018-06-02 14:32:16 +0530

    done

--- src/mobileauto/calculator.java
+++ src/mobileauto/calculator.java
@@ -2,7 +2,7 @@
 //Again checking
 //this is second time
 package mobileauto;
-
+//again
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,25 +10,38 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
-
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class calculator {
 
 	
         DesiredCapabilities capabilities;
-        AndroidDriver driver;
+         WebDriver driver;
+         AppiumDriverLocalService appiumService;
+         
 
         @BeforeTest
-        public void beforeTest() throws FileNotFoundException, IOException {
+        public void setup() throws MalformedURLException {
+        String Appium_Node_Path="C:/Program Files/Appium/node.exe";
+        String Appium_JS_Path="C:/Program Files/Appium/node_modules/appium/bin/appium.js";
+        //AppiumDriverLocalService appiumService;
+
+        appiumService = AppiumDriverLocalService.buildService(new AppiumServiceBuilder().
+        	                usingAnyFreePort().usingDriverExecutable(new File(Appium_Node_Path)).
+        	                withAppiumJS(new File(Appium_JS_Path)));
+        appiumService.start();
+        }
+        @Test
+        public void beforeTest() throws FileNotFoundException, IOException,NullPointerException {
                         
                         String currentdir = System.getProperty("user.dir");
                         File dir = new File(currentdir);
@@ -36,7 +49,7 @@ public class calculator {
                         parentpath = parentpath.replace('\\', '/');
                         
                         Properties properties = new Properties();
-                        properties.load(new FileInputStream(parentpath+ "/src/Config.properties"));
+                        properties.load(new FileInputStream(parentpath+ "/src/config.properties"));
 
                         capabilities = new DesiredCapabilities();
                         capabilities.setCapability("deviceName", properties.getProperty("deviceName"));
@@ -45,20 +58,16 @@ public class calculator {
                         capabilities.setCapability("appPackage", properties.getProperty("appPackage"));
                         capabilities.setCapability("appActivity", properties.getProperty("appActivity"));
                         try {
-                                        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+                                        driver = new  RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
                         } catch (MalformedURLException e) {
                                         // TODO Auto-generated catch block
                                         e.printStackTrace();
                         }
         }
         @Test
-        public void InvalidEmailId() throws MalformedURLException, InterruptedException {
-                        //Thread.sleep(1000);
-                        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-                        driver.findElementById("com.cgi.defera:id/login_user_edit").sendKeys("android.inteter@example.com");
-                        driver.findElement(By.id("com.cgi.defera:id/login_pass_edit")).sendKeys("Temp1234");
-                        driver.findElement(By.id("com.cgi.defera:id/login_btn_login")).click();
-                                        }
+        public void perform() {
+        	
+        }
 
 
 
